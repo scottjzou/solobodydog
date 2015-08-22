0e0574dea3e17f305bc6a8340827d112e6e5cae2
Hanyu Zhang <hyzhangsf@gmail.com>
2015-08-22 14:46:24 -0700

    added index, login, profile, signup pages

--- solobodydog/app/urls.py
+++ solobodydog/app/urls.py
@@ -1,1 +1,19 @@
-__author__ = 'zz'
+from django.conf.urls import url
+from django.conf.urls import include, url
+from . import views
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    url(r'^index/', views.index, name='index'),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^users', views.users, name='users'),
+    url(r'^login', views.login, name='login'),
+    url(r'^profile', views.my_profile, name='my_profile'),
+    url(r'^logout', views.logout, name='logout'),
+    url(r'^signup', views.signup, name='signup'),
+
+
+    # url('^', include('django.contrib.auth.urls')),
+]
+
